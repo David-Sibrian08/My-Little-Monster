1ebb1928a6e3173eb5946b1aaaef3e3917e2d36a
David Sibrian <davidsibrian08@gmail.com>
2016-07-18 18:10:41 -0400

    Animated the golem
    Used UIKit animationImages and a series of UIImages to achieve the
    desired effect

--- My Little Monster/ViewController.swift
+++ My Little Monster/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var monsterImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupAndAnimateImage()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func setupAndAnimateImage() {
+        var imgArray = [UIImage]()
+        
+        for i in 1...4 {
+            let image = UIImage(named: "idle\(i).png")
+            imgArray.append(image!)
+        }
+        
+        monsterImage.animationImages = imgArray
+        monsterImage.animationDuration = 0.8
+        monsterImage.animationRepeatCount = 0
+        monsterImage.startAnimating()
+    }
 
 }
 
